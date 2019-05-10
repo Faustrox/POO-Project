469ad9594403a80a44fa0b17e607c7b4a0a46063
Fausto Jaquez <faustojr03@hotmail.com>
2019-05-10 18:44:53 -0400

    Game loop subdivisions fixed

--- game_loop.py
+++ game_loop.py
@@ -137,7 +137,7 @@ def move_select(piece_selected, position_name, turn):
     move_to = str(move_to)
 
     if move_to[0] in minus: # Lo mismo que arriba, si incertas una letra en minuscula lo convierte a mayuscula
-    move_to = chr(ord(move_to[0]) - 32) + move_to[1]
+        move_to = chr(ord(move_to[0]) - 32) + move_to[1]
 
     return move_to
 
@@ -173,82 +173,7 @@ while True: # BASE LOOP (GAME LOOP)
     elif check(team[turn]) == "check": # La condicion del jaque
 
         print("The ", team[turn], " is in check")
-lass pawn(piece):
-    # Hereda los atributos de equipo y posiciones
-    def __init__(self, pos, team, name):
-        super().__init__(pos, team, name)
-        self.first_turn = True
 
-    def possible_move(self):
-        # Movimientos posibles
-        dic = {"Forward": [], "Right": [], "Left": []}
-        pos = [int(self.pos[1]), board.positionY[self.pos[0]]]
-        move_RD, move_LD = False, False
-
-        # Verifica si el jugador usa fichas negras o blancas
-        if self.team == "black":
-            move_forward = pos
-            move_forward[0] += 1
-
-        elif self.team == "white":
-            move_forward = pos
-            move_forward[0] -= 1
-
-        # desplazamiento de las posiciones de las fichas
-        if pos[1] >= 1 and pos[1] < 8:
-            move_RD = [move_forward[0], move_forward[1] + 1]
-
-        if pos[1] > 1 and pos[1] <= 8:
-            move_LD = [move_forward[0], move_forward[1] - 1]
-
-        if isinstance(board.game_board[move_forward[0]][move_forward[1]], piece) is False:
-
-            dic["Forward"].append(self.positionY[move_forward[1]] + str(move_forward[0]))
-
-            # Verifica el primer movimiento de cada peon
-
-            if self.pos[1] == "2" and self.team == "black":
-                dic["Forward"].append(self.positionY[move_forward[1]] + str(move_forward[0] + 1))
-            elif self.pos[1] == "7" and self.team == "white":
-                dic["Forward"].append(self.positionY[move_forward[1]] + str(move_forward[0] - 1))
-
-        # Verifica el movimiento diagonal para poder comer o eliminar una ficha enemiga
-        if move_RD is not False:
-            # derecha
-
-            if isinstance(board.game_board[move_RD[0]][move_RD[1]], piece) is True:
-
-                dic["Right"].append(self.positionY[move_RD[1]] + str(move_RD[0]))
-
-        if move_LD is not False:
-            # Izquierda
-
-            if isinstance(board.game_board[move_LD[0]][move_LD[1]], piece) is True:
-
-                dic["Left"].append(self.positionY[move_LD[1]] + str(move_LD[0]))
-
-        return dic
-
-    def __str__(self):  # function para que no imprima en lenguaje maquina
-
-        if self.team == "white":
-            simbol = "♙"
-
-        else:
-            simbol = "♟"
-
-        return simbol
-    #     # A pawn move one step by one but in the exit it can do two step in one move.
-
-    def switch(self): # Funcion del peon para cambiar de pieza si llega a la base enemiga
-
-        if self.team == "white":
-
-            destination = 1
-
-        else:
-
-destination = 8
     piece_select = pieceSelected()
 
     if piece_select == False: # Si la posicion que escribiste no esta en la matriz entonces se repetira la seleccion de alguna pieza
